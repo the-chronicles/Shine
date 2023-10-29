6ddc8d840e9ebca25bdb5d0776a028ef6005fd57
the-chronicles <oluseungideon@gmail.com>
2023-10-29 03:45:04 +0100

    Setting up the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 export default function App() {
   return (
     <div>
-      <Header />
+      <Navbar>
+        <NavLinks />
+      </Navbar>
     </div>
   );
 }
 
-function Header() {
-  return <h1>Shine</h1>;
+function Navbar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span>🔵</span>
+      <h2>shine</h2>
+    </div>
+  );
+}
+
+function NavLinks() {
+  return (
+    <div className="nav-link">
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Service</a>
+        </li>
+        <li>
+          <a href="#">Bookings</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
